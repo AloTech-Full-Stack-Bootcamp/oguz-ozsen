d23566a69c9608674507005b98ddf6fc9582ac6d
Oguz Ozsen <aslanoguz_1101@hotmail.com>
2021-12-09 18:11:11 +0300

    random number fix infinite loop

--- Week7/random_number.py
+++ Week7/random_number.py
@@ -5,19 +5,27 @@ digit = int(input("Basamak sayısını giriniz: "))
 
 
 def random_number_generator(count, digit=6):
-  radVal = "1"
-  numbers = set()
-  if (digit==1):
-    radVal = 10
+  #istenilen basamak ile üretilebilecek maksimum sayıyı hesaplıyoruz
+  valRange = (((10**digit)-1)-(10**(digit-1)))
+  #eğer maksimum üretilen sayı istenilen sayıdan büyük veya eşit ise içeri giriyoruz 
+  if(valRange >= count):
+    radVal = "1"
+    numbers = set()
+    if (digit==1):
+      radVal = 10
+    else:#istenilen basamak kadar 0 ekliyoruz
+      for _ in range(digit):
+        radVal = radVal + "0"
+    #stringi int e çeviriyoruz işlem yaparken hata almamak için
+    radVal = int(radVal)
+    #setin içindeki değer istenisel sayı miktarından küçükse döngü sürekli çalışacak
+    while(len(numbers)<count):
+      value = randint(radVal/10-1 if radVal==10 else radVal/10 , radVal-1)
+      numbers.add(value)
+    print(numbers)
+  #değil ise hata atıyoruz ve maksimum üretilebilcek sayıyı bastırıyoruz
   else:
-    for _ in range(digit):
-      radVal = radVal + "0"
+    print(f"Hata, istenilen basamak sayısı {digit} bu basamak sayısı size maksimum {valRange} sayı üretebilir.")
   
-  radVal = int(radVal)
-  while(len(numbers)<count):
-    value = randint(radVal/10-1 if radVal==10 else radVal/10 , radVal-1)
-    numbers.add(value)
-      
-  print(numbers)
 
 random_number_generator(count, digit)
